bed506e21d36a63c8bac200d538d54ee9bd08146
infOdysseus <infodysseus@gmail.com>
2021-06-01 20:39:23 +0900

    add department notice

--- src/home/components/Notice/Notice.js
+++ src/home/components/Notice/Notice.js
@@ -1,74 +1,32 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios"
+import NoticeForm from "../NoticeForm/NoticForm"
 import "./Notice.scss";
 
 function Notice() {
+  const [generalNotiList, setGeneralNotiList] = useState(null);
+  const [schoolNotiList, setSchoolNotiList] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    fetch("https://calm-mesa-43659.herokuapp.com/notification")
-      .then((res) => res.json())
-      .then((data) => {
-        setGeneralNotiList(data.generalNotiList.slice(0, 4));
-        setSchoolNotiList(data.schoolNotiList.slice(0, 4));
-        setIsLoading(false);
+    // 일반공지
+    axios.post("http://52.79.130.113:3000/kaunotices/",{bbsId : "0119", bbsAuth : "30", pageIndex:1, siteFlag:"www"}, {params : {category : "general"}})
+      .then((res) => {
+        setGeneralNotiList({list : res.data.result, gcId : "gc32172b.do"})
       });
   }, []);
-  const [generalNotiList, setGeneralNotiList] = useState([]);
-  const [schoolNotiList, setSchoolNotiList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const isNewItem = (item) => {
-    if (
-      (new Date(item.date).getDate() === new Date().getDate() ||
-        new Date(item.date).getDate() === new Date().getDate() - 1) &&
-      new Date(item.date).getMonth() === new Date().getMonth()
-    ) {
-      return "new";
-    } else {
-      return "old";
-    }
-  };
+  useEffect(() => {
+    // 학사공지
+    axios.post("http://52.79.130.113:3000/kaunotices/",{bbsId : "0120", bbsAuth : "30", pageIndex:1, siteFlag:"www"}, {params : {category : "general"}})
+      .then((res) => {
+        setSchoolNotiList({list : res.data.result, gcId : "gc14561b.do"})
+      });
+  }, []);
 
   return (
     <div className="Notice">
-      <nav className="Notice__general">
-        <div style={{ fontSize: "1.2rem", fontWeight: 700 }}>
-          일반공지{"  "}
-          <span style={{ fontSize: "0.8rem", fontWeight: 400 }}>
-            General Notice
-          </span>
-        </div>
-        <div className="Notice__border"></div>
-        <div className="Notice__container">
-          {isLoading
-            ? "로딩 중..."
-            : generalNotiList.map((item, idx) => (
-                <article key={idx} className={`article__${isNewItem(item)}`}>
-                  <a href={"https://www.kau.ac.kr/web/pages/gc32172b.do"}>
-                    {item}
-                  </a>
-                </article>
-              ))}
-        </div>
-      </nav>
-      <nav className="Notice__academy">
-        <div style={{ fontSize: "1.2rem", fontWeight: 700 }}>
-          학사공지{"  "}
-          <span style={{ fontSize: "0.8rem", fontWeight: 400 }}>
-            Academic Notice
-          </span>
-        </div>
-        <div className="Notice__border"></div>
-        <div className="Notice__container">
-          {isLoading
-            ? "로딩 중..."
-            : schoolNotiList.map((item, idx) => (
-                <article key={idx} className={`article__${isNewItem(item)}`}>
-                  <a href={"https://www.kau.ac.kr/web/pages/gc14561b.do"}>
-                    {item}
-                  </a>
-                </article>
-              ))}
-        </div>
-      </nav>
+
+      <NoticeForm noticeInfo={{korean : "일반공지", english : "General Notice"}} noticeList={generalNotiList} category={"general"}/>
+      <NoticeForm noticeInfo={{korean : "학사공지", english : "Academic Notice"}} noticeList={schoolNotiList} category={"general"}/>
     </div>
   );
 }
