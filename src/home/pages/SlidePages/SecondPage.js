34d669c344e5417dffeaed80fd65dcf67843364e
daehwa <daehwa00@naver.com>
2022-02-16 00:13:27 +0900

    design second page, and fixed login-error message

--- src/home/pages/SlidePages/SecondPage.js
+++ src/home/pages/SlidePages/SecondPage.js
@@ -1,48 +1,64 @@
-import axios from 'axios'
-import {useState, useEffect} from 'react'
-import address from "../../../address-info"
+import axios from "axios";
+import { useState, useEffect } from "react";
+import address from "../../../address-info";
+import "./SecondPage.scss";
 
-function SecondPage(){
-  const [newsContent, setNewsContent] = useState([])
+function SecondPage() {
+  const [newsContent, setNewsContent] = useState([]);
 
   useEffect(() => {
-  axios.get(address.url + '/api/kaunews')
-  .then(response=> {
-    let div = document.createElement('div')
-    div.innerHTML = response.data.result
-    let contentData = div.querySelector('#resultBody')
-    let titles = Array.from(contentData.querySelectorAll('tr td:nth-child(2)')).slice(0,3)
-    const newsList = []
-    Array.from(contentData.querySelectorAll('tr td:nth-child(4)')).slice(0,3).map((elem, index)=>{
-      console.log(elem.querySelector('p').innerText)
-      newsList.push({
-        "src":elem.querySelector('p img').src.replace("localhost:3000", "old.kau.ac.kr"), 
-      "content":elem.querySelector('p').innerText,
-    "title" : titles[index].innerText})
-      })
-      setNewsContent(newsList)
-  })
-  },[])
-    return (
-    <div className="second">
-      { newsContent.map((elem,idx)=>{
-        console.log(elem)
-      return (
-      <div className="section" key={idx}>
-        <div className="section-wrapper">
-          <div className="vertical-wrapper">
-            <div className="img-wrapper">
-              <img src={elem["src"]}></img>
+    axios.get(address.url + "/api/kaunews").then((response) => {
+      let div = document.createElement("div");
+      div.innerHTML = response.data.result;
+      let contentData = div.querySelector("#resultBody");
+      let titles = Array.from(
+        contentData.querySelectorAll("tr td:nth-child(2)")
+      ).slice(0, 3);
+      const newsList = [];
+      Array.from(contentData.querySelectorAll("tr td:nth-child(4)"))
+        .slice(0, 3)
+        .map((elem, index) => {
+          newsList.push({
+            src: elem
+              .querySelector("p img")
+              .src.replace("localhost:3000", "old.kau.ac.kr"),
+            content: elem.querySelector("p").innerText,
+            title: titles[index].innerText,
+          });
+        });
+      setNewsContent(newsList);
+    });
+  }, []);
+  return (
+    <div className="card-container">
+      {newsContent.map((elem, idx) => {
+        return (
+          <div className="card" key={idx}>
+            <div className="card-top">
+              <a href="">
+                <img src={elem["src"]} alt="alt" />
+              </a>
             </div>
-            <div className="text-contents">
-              <div className="title-wrapper">{elem["title"]}</div> 
-              <div className="text-wrapper">{elem["content"]}</div>
-            </div> 
+            <div className="card-content">
+              <h6 className="tag">KAU</h6>
+              <a href="">
+                <div className="title">
+                  {elem["title"].length < 22
+                    ? elem["title"]
+                    : `${elem["title"].slice(0, 22)}...`}
+                </div>
+              </a>
+              <p>
+                {elem["content"].length < 70
+                  ? elem["content"]
+                  : `${elem["content"].slice(0, 70)}...`}
+              </p>
+            </div>
           </div>
-        </div>
-      </div>)
-    }) }
-  </div>)
+        );
+      })}
+    </div>
+  );
 }
 
-export default SecondPage
+export default SecondPage;
